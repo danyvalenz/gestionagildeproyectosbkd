edb01c82b25f6969fe889c50dd1c89be9442d222
Daniel Valenzuela <30942529+danyvalenz@users.noreply.github.com>
2025-02-28 16:10:50 -0700

    proyecto base
    proyecto base

---
+++ src/main/java/com/gan/gestionagildenegociobkd/GestionagildenegociobkdApplication.java
@@ -0,0 +1,13 @@
+package com.gan.gestionagildenegociobkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionagildenegociobkdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionagildenegociobkdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gan/gestionagildenegociobkd/GestionagildenegociobkdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gan.gestionagildenegociobkd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionagildenegociobkdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
